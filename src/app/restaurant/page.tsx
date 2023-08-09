d9f8d73a06aa1decd40bc9f5ab0fd23d71abbb8b
ShariarSheikh <shariar.dev@gmail.com>
2023-08-09 15:35:33 +0600

    restaurant page responsive design done

--- src/app/restaurant/page.tsx
+++ src/app/restaurant/page.tsx
@@ -18,14 +18,14 @@ export default function Restaurant() {
             Hukka Bukka
           </Link>
 
-          <div className="flex flex-col items-center">
-            <h2 className="text-[#FACE8D] font-dancing text-[80px] leading-none">
+          <div className="w-full flex flex-col items-center">
+            <h2 className="text-[#FACE8D] font-dancing text-[50px] md:text-[80px] leading-none">
               The pure taste of
             </h2>
-            <h1 className="font-medium text-[160px] leading-none">
+            <h1 className="font-medium text-[55px] md:text-[120px] lg:text-[160px] leading-none">
               Bangladesh
             </h1>
-            <p className="text-[23px] mt-[20px] max-w-[600px]">
+            <p className="text-[20px] md:text-[23px] text-center md:text-start mt-[20px] max-w-[600px]">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore.
             </p>
